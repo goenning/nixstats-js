1dc377cfefdafbf858d630c13b1fdf46eb2f1f28
goenning <oenning.ti@gmail.com>
2016-06-17 06:03:05 -0300

    fix homepath for unix

--- bin/config-store.js
+++ bin/config-store.js
@@ -3,8 +3,9 @@
 var fs = require('fs');
 var path = require('path');
 
+var sep = (process.platform == 'win32') ? '\\' : '/';
 var homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-var configFile = `${homeDir}\\.config\\nixstats.json`;
+var configFile = `${homeDir}${sep}.config${sep}nixstats.json`;
 
 function ensureDirectoryExistence(filePath) {
   var dirname = path.dirname(filePath);
